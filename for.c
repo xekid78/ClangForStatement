f828a5052aaf631158f02d96e17ffc869238cbae
Makoto Tanabe <xekid78@outlook.com>
2018-03-20 21:57:33 +0900

    Add files via upload

--- for.c
+++ for.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
-int main(int argc, char const *argv[]) {
+int main(void) {
     int i;
     for (i = 1; i <= 12; i++) {
-        printf("%d月\n",i );
+        printf("%d月\n",i);
     }
     return 0;
 }
